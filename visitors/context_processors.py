288d17171235f12fe8a2a0d38a89519d659a11b2
Hugo Rodger-Brown <hugo@yunojuno.com>
2021-07-27 16:43:51 +0100

    Fix context processor

--- visitors/context_processors.py
+++ visitors/context_processors.py
@@ -1,15 +1,10 @@
 from __future__ import annotations
 
 from django.http import HttpRequest
-from django.utils.functional import SimpleLazyObject
 
 
-def visitor(request: HttpRequest) -> dict[str, SimpleLazyObject]:
+def visitor(request: HttpRequest) -> dict[str, object]:
     """Add visitor context to template context (if found on the request)."""
-
-    def _get_val() -> dict | None:
-        if request.visitor:
-            return request.visitor.serialize()
-        return None
-
-    return {"visitor": SimpleLazyObject(_get_val)}
+    if not request.visitor:
+        return {"visitor": None}
+    return {"visitor": request.visitor.serialize()}
