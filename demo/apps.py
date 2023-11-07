604e09b8c19addbb44d7f10aaa2027c3e7cf8fd7
Hugo Rodger-Brown <hugorodgerbrown@users.noreply.github.com>
2023-11-07 11:06:03 +0000

    Update Django / Python support (#14)
    Drop Django 3.1, add Django 4, 5.

--- demo/apps.py
+++ demo/apps.py
@@ -2,7 +2,6 @@ from django.apps import AppConfig
 
 
 class DemoConfig(AppConfig):
-
     name = "demo"
 
     def ready(self) -> None:
--- visitors/migrations/0006_alter_visitor_uuid.py
+++ visitors/migrations/0006_alter_visitor_uuid.py
@@ -6,7 +6,6 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ("visitors", "0005_visitorlog_status_code"),
     ]
